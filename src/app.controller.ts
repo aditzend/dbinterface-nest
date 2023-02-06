25defc30a3f5ac9176574bf71e92a1cf37dced62
Alexander Ditzend <alex@saia.ar>
2023-02-06 12:04:02 -0300

    version

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
   @Get('version')
   getVersion(): string {
-    return 'Version 3 Branch: "incoming_task_params"';
+    return 'Version 3 Branch: "transcript-endpoints"';
   }
 }
